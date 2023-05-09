cf343d0aa22972ae5643e060e434318a51a094e1
vgd15 <deise171315@gmail.com>
2023-05-09 09:45:05 -0300

    Versão corrigida

--- PROJETO-BACKEND/bancoDeDados.js
+++ PROJETO-BACKEND/bancoDeDados.js
@@ -9,7 +9,7 @@ async function conectaBancoDeDados() {
 
     try {
         console.log("Conexão com o banco de dados iniciou")
-''
+
         await mongoose.connect(process.env.MONGO_URL)
 
         console.log('Conexão com o banco de dados feita com sucesso')
